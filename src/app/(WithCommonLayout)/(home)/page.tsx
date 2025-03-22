8782fae9b9c752d347d5ae50afb035d2841d6ec5
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-23 01:25:56 +0600

    Enhance authentication options; add profile handling for social providers and include authType in session data

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,13 +1,7 @@
 import HomeComponents from '@/components/modules/home';
-import { authOptions } from '@/utils/authOptions';
-import { getServerSession } from 'next-auth';
 import React from 'react';
 
-const HomePage = async () => {
-
-    // const session = await getServerSession(authOptions);
-    // console.log(session);
-
+const HomePage = () => {
 
     return (
         <div>
--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -8,11 +8,27 @@ export const authOptions: NextAuthOptions = {
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
+            profile(profile) {
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: profile.role ?? "student",
+                    authType : 'social'
+                }
+            },
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
         }),
 
         GoogleProvider({
+            profile(profile) {
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: profile.role ?? "student",
+                    authType : 'social'
+                }
+            },
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
@@ -49,6 +65,7 @@ export const authOptions: NextAuthOptions = {
                         name: decoded.name || null,
                         email: decoded.email || null,
                         role: decoded.role || null,
+                        authType : 'credentials'
                     };
                 }
                 return null;
@@ -62,15 +79,17 @@ export const authOptions: NextAuthOptions = {
             if (user) {
                 token.id = user.id;
                 token.role = user.role;
+                token.authType = user.authType;
             }
             return token;
         },
-        async session({ session, token}) {
+        async session({ session, token }) {
             if (session.user) {
                 session.user.id = token.id as string;
                 session.user.email = token.email as string;
                 session.user.name = token.name as string;
                 session.user.role = token.role as string;
+                session.user.authType = token.authType as string; 
             }
             // console.log(session);
             return session;
