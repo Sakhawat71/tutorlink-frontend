38dfe7dab917617e8bb42eb132f076f74ba2b076
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-04 19:21:43 +0100

    details page

--- src/app/(WithCommonLayout)/tutors/[id]/page.tsx
+++ src/app/(WithCommonLayout)/tutors/[id]/page.tsx
@@ -1,11 +1,15 @@
+"use client"
+
+import { useParams } from 'next/navigation';
 import React from 'react';
 
 const SubjectDetails = () => {
 
-    
+    const { id } = useParams();
+
     return (
         <div>
-            
+            <h2>Id : {id}</h2>
         </div>
     );
 };
