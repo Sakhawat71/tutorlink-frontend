5e6341e9a8819d645cc7aad5ee1efd8869c80f97
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-07-03 03:17:21 +0600

    Refactor booking components: remove BookATutor and implement BookSession with tutor data fetching and calendar integration

--- src/app/(WithCommonLayout)/tutors/[id]/book/page.tsx
+++ src/app/(WithCommonLayout)/tutors/[id]/book/page.tsx
@@ -1,9 +1,11 @@
+import BookSession from '@/components/modules/booking/BookSession';
 import React from 'react';
 
 const BookTutorPage = () => {
     return (
         <div>
             book a tutor page
+            <BookSession />
         </div>
     );
 };
