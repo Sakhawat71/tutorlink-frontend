b6c84b1adf52ea405f84cbaa2c7acb0c5a66dc5a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-07-16 23:19:21 +0330

    Remove StoreProvider and simplify Providers component

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,15 +1,12 @@
 "use client"
 
-import { SessionProvider } from "next-auth/react"
-import StoreProvider from "./StoreProvider";
+import { SessionProvider } from "next-auth/react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <SessionProvider>
-            <StoreProvider >
-                {children}
-            </StoreProvider>
+            {children}
         </SessionProvider>
     )
 };
