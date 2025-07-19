1259f2a73002ec2a45bffb8e08ab33a7d91d114f
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-07-19 20:51:12 +0600

    Add loading component for TutorsPage with spinner animation

---
+++ src/app/(WithCommonLayout)/tutors/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HashLoader } from 'react-spinners';
+
+const Tutuorloading = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <HashLoader />
+        </div>
+    );
+};
+
+export default Tutuorloading;
--- src/app/(WithCommonLayout)/tutors/page.tsx
+++ src/app/(WithCommonLayout)/tutors/page.tsx
@@ -1,5 +1,11 @@
 import BrowseTutors from '@/components/modules/browseTutors/BrowseTutors';
+import { Metadata } from 'next';
 import React from 'react';
+
+export const metadata:Metadata = {
+    title: 'TutorLink | Tutors',
+    description: 'Browse and connect with tutors',
+}
 
 const TutorsPage = async () => {
 
